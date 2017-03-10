c4c77731aeb0a83f2efc90b27718d03e175286c1
theSunAlwaysRise <fg352l@sina.com>
2017-03-10 18:21:49 +0800

    v1
    bv1

---
+++ res/js/js.js
@@ -0,0 +1,13 @@
+var nav=[];
+nav.push('<h1 class="logo"><a href="index.html"></a></h1><div class="nav">');
+nav.push('<a class="n1" href="index.html"><span>home</span><b>首页</b><i></i></acl>');
+nav.push('<a class="n1" href="case.html"><span>case</span><b>婚礼</b><i></i></a>');
+nav.push('<a class="n1" href="kids.html"><span>kids</span><b>宝宝宴</b><i></i></a>');
+nav.push('<a class="n2" href="design.html"><span>design</span><b>策划团队</b><i></i></a>');
+nav.push('<a class="n2" href="about_us.html"><span>about us</span><b>关于伯妮</b><i></i></a>');
+nav.push('<a class="n3" href="contact_us.html"><span>conatct us</span><b>联系我们</b><i></i></a>');
+nav.push('<a class="n4" href="leave_message.html"><span>leave message</span><b>给我留言</b><i></i></a>');
+nav.push('<a class="n1" href="join_us.html"><span>join us</span><b>加盟合作</b></a><div id="music"></div></div>');
+document.getElementById('header').innerHTML=nav.join('');
+
+
